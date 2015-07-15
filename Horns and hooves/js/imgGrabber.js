bb0fc6dc5823ba10550b6b00336e3325cba45bbf
Yuriy Kurenkov <yuramax@gmail.com>
2015-07-15 23:06:52 +0300

    Implementation of downloading images by clicking on them

--- Horns and hooves/js/imgGrabber.js
+++ Horns and hooves/js/imgGrabber.js
@@ -2,45 +2,29 @@
 addEventListener(window, "onload",
  function () {
 
-	var container = document.getElementById("wrapper");
+		var imgs = document.getElementsByTagName('img'),
+    L = imgs.length,
+    i, tmp;
+ 
+		for (i = 0; i < L; i++) {
 
-	var imgArray = container.getElementsByTagName('img');
+				var src = imgs[i].getAttribute("src");
 
+		    tmp = document.createElement('a');
+		    tmp.setAttribute("class", "download");
+		    tmp.setAttribute("href", imgs[i].src);
+		    tmp.setAttribute("download", "");
 
-	for(var i=0; i < imgArray.length; i++) {
-		// var src = imgArray[i].getAttribute("src");
-	    var img = imgArray[i];
-		var button = document.createElement('a');
-		var text = document.createTextNode("download");
-		button.appendChild(text);
-		button.setAttribute("class", "downloadBtn");
-		button.setAttribute("href", img.src);
-		button.setAttribute("download", "");
+		    imgs[i].parentNode.insertBefore(tmp, imgs[i]);
+		    tmp.appendChild(imgs[i]);
 
-		button.onclick = function (e) {
-		    if (!e) {
-		        window.event.cancelBubble = true;
-		    } else if (e.stopPropagation) {
-		        e.stopPropagation();
-		    }
-		}
+		    var button = document.createElement('div');
+		    button.setAttribute("class", "downloadMe");
 
-        imgArray[i].parentNode.insertBefore(button, img);
+		    var text = document.createTextNode("download");
+		    button.appendChild(text);
+		    tmp.appendChild(button);
 
-		addEventListener(img, "onmouseover", (function (div) {
-			return function(){
-				div.classList.add("enabled");
-			}
-		}
-		)(button));
-
-
-		addEventListener(imgArray[i], "onmouseout", (function(div) {
-			return function() {
-				div.setAttribute("class", "downloadBtn");
-			}
-
-		})(button));
-	}
+		};
 
  });
