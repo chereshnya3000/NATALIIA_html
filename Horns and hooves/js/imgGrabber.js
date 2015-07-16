4af4eb983ff8a6317eb9e8f5dd0b2944b647748c
Yuriy Kurenkov <yuramax@gmail.com>
2015-07-16 22:13:39 +0300

    the latest changes on download images

--- Horns and hooves/js/imgGrabber.js
+++ Horns and hooves/js/imgGrabber.js
@@ -5,14 +5,12 @@ addEventListener(window, "onload",
 		var imgs = document.getElementsByTagName('img'),
     L = imgs.length,
     i, tmp;
- 
-		for (i = 0; i < L; i++) {
 
-				var src = imgs[i].getAttribute("src");
+		for (i = 0; i < L; i++) {
 
 		    tmp = document.createElement('a');
 		    tmp.setAttribute("class", "download");
-		    tmp.setAttribute("href", imgs[i].src);
+		    tmp.setAttribute("href", imgs[i].getAttribute("src"));
 		    tmp.setAttribute("download", "");
 
 		    imgs[i].parentNode.insertBefore(tmp, imgs[i]);
